4c4736d3933a49d474e7f44d433457c1f387317c
Kevin Baktiar <afterafx@entgwgtm.lb.intuit.com127.0.0.1>
2019-06-25 15:07:35 -0700

    fixed npm vulnerability

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const kevin = { name: 'Kevin', age: 29, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(kevin);
+  // res.send(req.query.name);
+  res.json(req.query);
 });
 
 module.exports = router;
